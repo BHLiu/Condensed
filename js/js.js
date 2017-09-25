9f4585046a6404189acd501f085b230cb94e82d1
Brandon <30844989+Restok@users.noreply.github.com>
2017-09-24 19:56:37 -0700

    A ton of stuff
    A tooonnn of stuff

--- js/js.js
+++ js/js.js
@@ -1,7 +1,52 @@
+alert("This website was developed in 1920x1080, and has been innovated to fit 1280x720. If your dimensions are anything in between...Man up and scroll.")
+$(function(){
+    $('#lay').hide();
+});
+$(function(){
+    $('#some').hide();
+});
 $(window).on('load', function(){
-    $('#quotes').hide().fadeIn(700);
+    $('#quot').hide();
+});
+$(window).on('load', function(){
+    $('#quotes').hide().fadeIn(1000, function(){
+        $('#quot').fadeIn(800);
+    });
 })
 
-$(window).on('load', function(){
-    $('#quot').delay(500).hide().fadeIn(800);
+$(function() {
+    $('#fill').hover(function() { 
+        $('#fill').fadeTo("fast", 1); 
+    });
+    });
+    $(function() {
+        $('.b2').hover(function() { 
+            $('.b2').fadeTo("slow", 1); 
+        });
+        });
+
+        $(function() {
+            $('.b3').hover(function() { 
+                $('.b3').fadeTo("slow", 1); 
+            });
+            });
+
+            $(function() {
+                $('.b4').hover(function() { 
+                    $('.b4').fadeTo("slow", 1); 
+                });
+                });
+                $(function() {
+                    $('.b5').hover(function() { 
+                        $('.b5').fadeTo("slow", 1); 
+                    });
+                    });
+
+
+$(function(){
+    $('#endbut').click(function(){
+        $('#lay').fadeTo('slow', 0.7).delay(800, function(){
+            $('#some').fadeTo('slow', 1);
+        });
+    })
 })
